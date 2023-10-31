7ecf7f4c2fd957fd319800f03fad7917642df364
Umma hafsa labonno <139877247+2021-Labonno@users.noreply.github.com>
2023-10-31 23:05:40 +0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+    alert("আসসালামু আলাইকুম। আপনাকে U.H.L এর ওয়েবসাইটে স্বাগতম। 🥰");
+  }
